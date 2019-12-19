a9e38d829ed9ddd4ba99327c47064a4b182d0672
wangmengmeng <wmm_0165@163.com>
2019-12-19 10:44:43 +0800

    住院干预理由测试用例

--- testcase/v3_5_191231/test_ganyu.py
+++ testcase/v3_5_191231/test_ganyu.py
@@ -1,6 +1,59 @@
 # -*- coding: utf-8 -*-
 # @Time : 2019/12/17 16:44
 # @Author : wangmengmeng
+import pytest
+
+
 class TestGanyuIpt:
     """住院干预双签理由展示测试用例"""
-    pass
+
+    def test_wait_ipt_01(self, zy):
+        """待审页面当前任务"""
+        zy.send.send('ipt', 'audit673_1', 1)
+        zy.send.send('ipt', 'audit673_2', 1)
+        engineid = zy.get_engineid(1)
+        res = zy.mergeEngineMsgList(engineid, 0, zy.send.change_data['{{gp}}'])
+        actual = len(res['data']['groupAudits'])
+        expected = 1
+        assert actual == expected
+
+    def test_wait_ipt_02(self, zy):
+        """待审页面当前任务"""
+        zy.send.send('ipt', 'audit673_5', 1)
+        engineid = zy.get_engineid(1)
+        res = zy.mergeEngineMsgList(engineid, 0, zy.send.change_data['{{gp}}'])
+        actual = len(res['data']['groupAudits'])
+        expected = 1
+        assert actual == expected
+
+    def test_ipt_merge_01(self, zy):
+        """待审页面合并任务"""
+        zy.send.send('ipt', 'audit673_1', 1)
+        zy.send.send('ipt', 'audit673_2', 1)
+        zy.send.send('ipt', 'audit673_3', 1)
+        engineid2 = zy.get_engineid(2)
+        res = zy.mergeEngineMsgList(engineid2, 0, zy.send.change_data['{{gp}}'])
+        actual = len(res['data']['groupAudits'])
+        expected = 1
+        assert actual == expected
+
+    def test_ipt_merge_02(self, zy):
+        """待审页面合并任务"""
+        zy.send.send('ipt', 'audit673_2', 1)
+        zy.send.send('ipt', 'audit673_4', 1)
+        zy.send.send('ipt', 'audit673_3', 1)
+        engineid2 = zy.get_engineid(2)
+        res = zy.mergeEngineMsgList(engineid2, 0, zy.send.change_data['{{gp}}'])
+        actual = res['data']['groupAudits']
+        expected = None
+        assert actual == expected
+
+    def test_ipt_modify(self,zy):
+        zy.send.send('ipt', 'audit673_1', 1)
+        zy.send.send('ipt', 'audit673_2', 1)
+        zy.send.send('ipt', 'audit673_6', 1)
+        engineid1 = zy.get_engineid(1)
+        res = zy.mergeEngineMsgList(engineid1, 0, zy.send.change_data['{{gp}}'])
+        actual = res['data']['groupAudits']
+        expected = None
+        assert actual == expected
