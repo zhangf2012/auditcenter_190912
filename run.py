89e4843dd8b2fcafc40f502172c84427e5a31772
wangmengmeng <wmm_0165@163.com>
2020-03-24 17:07:44 +0800

    run.py

--- run.py
+++ run.py
@@ -5,4 +5,4 @@ import pytest
 
 if __name__ == '__main__':
     # pytest.main(['-vs','testcase/v4_0_200228/test_audit_512.py','--alluredir=/var/jenkins_home/workspace/auditcenter_v4_0/allure-results'])
-    pass
+    pytest.main()
