671fcfbd4e9887bcdf3b27a068be99a903ee3c4e
wangmengmeng <wmm_0165@163.com>
2020-03-24 16:17:29 +0800

    注释路径

--- run.py
+++ run.py
@@ -2,6 +2,6 @@
 # @Time : 2020/3/24 9:32
 # @Author : wangmengmeng
 import pytest
-from config.config import PROJ_PATH
+
 if __name__ == '__main__':
-    pytest.main(['-vs','--clean-alluredir','--alluredir=%allure-results'%PROJ_PATH,'testcase/v4_0_200228/test_audit_512.py'])
+    pytest.main(['-vs', '--clean-alluredir', '--alluredir=allure-results', 'testcase/v4_0_200228/test_audit_512.py'])
