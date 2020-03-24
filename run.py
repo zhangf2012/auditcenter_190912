0f6776d2dd066f84aa84f5a4cd3082bb024e6199
wangmengmeng <wmm_0165@163.com>
2020-03-24 09:40:48 +0800

    持续集成，增加run.py

---
+++ run.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Time : 2020/3/24 9:32
+# @Author : wangmengmeng
+import pytest
+
+
+if __name__ == '__main__':
+    pytest.main(['-vs','--clean-alluredir','--alluredir=allure-results','testcase/v4_0_200228/test_audit_721.py'])
